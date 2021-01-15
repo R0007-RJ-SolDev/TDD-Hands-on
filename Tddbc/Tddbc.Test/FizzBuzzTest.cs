80c3e554b273578d6bdbce9bbe20bab43f5a900d
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-01-15 13:15:47 +0900

    Theoryを利用することで三角測量お1テストメソッドで実装。

--- Tddbc/Tddbc.Test/FizzBuzzTest.cs
+++ Tddbc/Tddbc.Test/FizzBuzzTest.cs
@@ -5,23 +5,15 @@ namespace Tddbc.Test
 {
     public class FizzBuzzTest
     {
-        [Fact]
-        public void _1を渡すと文字列1を返す()
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        public void 数を文字列に変換する(int num, string expected)
         {
             // 準備 Arrange
             var fizzBuzz = new FizzBuzz();
             // 実行 & 検証
-            Assert.Equal("1", fizzBuzz.Convert(1));
+            Assert.Equal(expected, fizzBuzz.Convert(num));
         }
-
-        [Fact]
-        public void _2を渡すと文字列2を返す()
-        {
-            // 準備 Arrange
-            var fizzBuzz = new FizzBuzz();
-            // 実行 & 検証
-            Assert.Equal("2", fizzBuzz.Convert(2));
-        }
-
     }
 }
