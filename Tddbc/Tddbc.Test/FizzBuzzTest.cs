0d53d11c36a4acf388b9225fdc15b6b04abf3adc
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-01-15 11:14:52 +0900

    テストプロジェクトの作成と、xUnitの正常動作確認

---
+++ Tddbc/Tddbc.Test/FizzBuzzTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tddbc.Test
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false, "‚±‚ê‚ÍŽ¸”s‚·‚é‚Í‚¸");
+        }
+    }
+}
